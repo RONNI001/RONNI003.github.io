dbb7ef4aae77ef337e18549a1437b475664c2c6d
RONNI001 <jajankang1@gmail.com>
2023-10-25 20:01:18 +0700

    first commit

---
+++ src/myProject/configDB.java
@@ -0,0 +1,31 @@
+package myProject;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class configDB {
+    private static Connection mysqlConfig;
+    
+    public static Connection koneksi(){
+        try{
+            String url = "jdbc:mysql://localhost:3306/pemrograman_2";
+            //nama databse yang login akademikdb
+            String user = "root";
+            String pass = "";
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            mysqlConfig = DriverManager.getConnection(url, user, pass);
+        } catch (SQLException e){
+            System.err.println("gagal koneksi "+e.getMessage());
+        }
+        return mysqlConfig;
+    }
+}
